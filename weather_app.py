e938f4d976187ba2f8b363c428e142f0e9e2a0c6
lian-0429 <aaa0970807313@gmail.com>
2026-06-26 12:54:25 +1000

    weather_app.py
    - switch from wttr.in to CWA API with robust error handlin
    -

--- weather_app.py
+++ weather_app.py
@@ -1,18 +1,45 @@
 import streamlit as st
 import requests
 
-st.title('天氣查詢APP')
-city = st.text_input('請輸入一個城市')
+url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
+API_KEY = "XXX"
 
-if city:
-    url = f'https://wttr.in/{city}?format=j1'
-    read = requests.get(url)
+def get_weather_cwa(city_name):
+    params = {
+        "authorization": API_KEY,
+        "location": city_name,
+        "format" : "JSON"
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    return None
 
-    if read.status_code == 200:
-        data = read.json()
-        temp = data['current_condition'][0]['temp_C']
-        desc = data['current_condition'][0]['weatherDesc'][0]['value']
-        st.success(f'目前{city}的溫度為{temp}，天氣狀況為{desc}')
-        st.info('謝謝您的查詢')
-    else:
-        st.error('查無此城市')
+def parse_weather_data(data):
+    try:
+        location = data['records']['location'][0]
+        weather_elements = location['weatherElement']
+        weather_desc = weather_elements[0]['time'][0]['parameter']['parameterName']
+        temp_min = weather_elements[2]['time'][0]['parameter']['parameterName']
+        temp_max = weather_elements[4]['time'][0]['parameter']['parameterName']
+        return f"天氣為 {weather_desc}，氣溫約 {temp_min}°C - {temp_max}°C"
+    except (KeyError, IndexError):
+        return "無法解析氣象資料，請檢查城市名稱是否正確。"
+
+
+st.title('天氣小管家')
+city = st.text_input('What city do you want to search')
+if st.button("查詢"): # 按下按鈕才開始跑
+
+    if city:  # 如果 city 不是空的
+        raw_data = get_weather_cwa(city) # 呼叫函數，就像派人去執行任務
+        if raw_data:  # 如果拿到了資料
+            result = parse_weather_data(raw_data)
+            st.write(result)
+            st.success('Your searching is success.')
+            # 繼續往下走
+        else:  # 如果沒拿到 (None)
+            st.error("API 連線失敗")  # 對應你的「報Error 查無資料」
+
+    else:  # 否則
+        st.warning("請先輸入縣市")  # 對應你的「報Error 查無城市」
